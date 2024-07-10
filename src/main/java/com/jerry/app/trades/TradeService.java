25a5b0d067aa4c1c1f88475c8f17a34587c92990
Jerry-0627 <mincand0627@gmail.com>
2024-07-10 14:01:20 +0900

    7.10_C5

--- src/main/java/com/jerry/app/trades/TradeService.java
+++ src/main/java/com/jerry/app/trades/TradeService.java
@@ -3,15 +3,27 @@ package com.jerry.app.trades;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jerry.app.account.AccountDAO;
+import com.jerry.app.account.AccountDTO;
+
 @Service
 public class TradeService {
 
 	@Autowired
 	private TradeDAO tradeDAO;
 
+	@Autowired
+	private AccountDAO accountDAO;
+
 	public int trade(TradeDTO tradeDTO) throws Exception {
 		// 1720516514277 계좌에서 5000원을 빼서 1720516604660에 보내기
-		tradeDTO.setTrade_amount(tradeDTO.getTrade_amount() * -1);
+		AccountDTO accountDTO = new AccountDTO();
+		accountDTO.setAccount_num(tradeDTO.getAccount_num());
+		accountDTO = accountDAO.detail(accountDTO);
+
+		if (accountDTO.getBalance() < tradeDTO.getTrade_amount()) {
+			return 0;
+		}
 		tradeDTO.setTrade_type("출금");
 
 		// 거래 내역을 추가
@@ -21,8 +33,7 @@ public class TradeService {
 		result = tradeDAO.update(tradeDTO);
 
 		// 받는 DTO 수정
-		tradeDTO.setAccount_num(tradeDTO.getReceive_num());
-		tradeDTO.setTrade_amount(tradeDTO.getTrade_amount() * -1);
+
 		tradeDTO.setTrade_type("입금");
 
 		// 받는 계좌의 거래 내역을 추가
